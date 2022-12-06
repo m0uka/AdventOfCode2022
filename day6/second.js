1e6a4421c6aa95cc674e7a79d2cdb9a8059e18b8
m0uka <lukasekmoucka0@gmail.com>
2022-12-06 10:14:57 +0100

    feat: day6

---
+++ day6/second.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const inputStr = fs.readFileSync('input.txt', 'utf8');
+const input = inputStr.split('');
+
+let bufferIndices = [];
+for (let i = 0; i < input.length; i++) {
+    const l = input[i];
+    for (let j = 0; j <= i; j++) {
+        if (!bufferIndices[j]) {
+            bufferIndices[j] = '';
+        }
+        if (bufferIndices[j].length === 14) continue;
+        bufferIndices[j] += l;
+    }
+};
+
+for (let i = 0; i < bufferIndices.length; i++) {
+    const buffer = bufferIndices[i];
+    const arr = buffer.split('');
+    const unique = new Set(arr).size === arr.length;
+
+    if (unique) {
+        console.log(buffer);
+        console.log(inputStr.indexOf(buffer) + 14);
+        break;
+    }
+};
